b9ce685c78af3540638dad8d2721a5a5ac289b73
devgaur109 <89730869+devgaur109@users.noreply.github.com>
2024-06-03 11:02:00 +0530

    Delete Semantic directory

--- Semantic/run.sh
+++
@@ -1,8 +0,0 @@
-yacc -d PWS42X5Y.y
-lex PWS42X5Y.l
-gcc y.tab.c lex.yy.c -ll
-./a.out input.txt
-pip install nltk
-python3 tree_vis.py
-
-echo -e "\n\n\n\n\n\nTo Properly Visualize the AST tree please run the jupyter notebook tree_vis.ipynb\n\n"
